8f47f2620e4b5e72220ae7a780560b96427ece2b
baptistemrt <contact@baptistemrt.fr>
2020-10-06 14:08:54 +0200

    Add progressbar component

---
+++ front/src/components/ProgressBar.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState} from 'react';
+
+import "../assets/scss/components/Home.scss";
+
+const ProgressBar = ({width, percent}) => {
+
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    setValue(percent * width);
+  });
+
+  return (
+    <div className="progress-block" style={{width: width}}>
+      <div className="progress" style={{width: `${value}px`}} />
+    </div>
+  );
+}
+
+export default ProgressBar;
