f28a68f6aa76c677c65e8baec37af4076d47954f
baptistemrt <contact@baptistemrt.fr>
2020-10-06 12:34:30 +0200

    Add the timeline

--- front/src/components/Home.js
+++ front/src/components/Home.js
@@ -1,27 +1,14 @@
 import React from 'react';
 
+import TimeLine from './TimeLine';
+
 import "../assets/scss/components/Home.scss";
 
 const Home = () => {
   return (
     <>
       <section className="content-cv">
-        <section className="graph">
-          <div className="pres">
-            <span>“</span>Étudiant depuis novembre 2019 à l'école d'informatique 42 et passionné d'informatique et de nouvelles technologies 
-            depuis le collège, ainsi que de programmation avec quelques créations disponible sur mon <a href="https://github.com/baptistemrt">GitHub</a>.<span>”</span>
-          </div>
-          <div>
-            <div className="left-content">
-              <div>
-              </div>
-            </div>
-            <div className="hr"></div>
-            <div className="right-content">
-
-            </div>
-          </div>
-        </section>
+        <TimeLine />
         <section className="skills">
           <h3>COMPÉTENCES</h3>
           <div>
